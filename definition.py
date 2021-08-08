f730f207f952dc54ddb4c19e4bbb669fb03c164e
Abhinav Awasthi <abhinavawasthikvgn@gmail.com>
2021-08-08 10:58:38 +0530

    implimented Definition class

---
+++ definition.py
@@ -0,0 +1,20 @@
+import pandas
+
+class Definition:
+
+    def __init__(self, term):
+        self.term = term.lower()
+
+    def get(self):
+        df = pandas.read_csv('dictionary.csv')
+        if len(df.loc[df['word']== self.term.title()]) != 0:
+            return tuple(df.loc[df['word']== self.term.title()]['meaning'])
+        else:
+            return f"sorry! no meaning found for {self.term}"
+
+        # return tuple(df.loc[df['word']== self.term.title()]['meaning'])
+
+
+user_input = input("Enter the word : ")
+obj = Definition(user_input)
+print(obj.get())
--- definiton.py
+++
@@ -1,8 +0,0 @@
-
-class Definiton:
-
-    def __init__(self, term):
-        self.term = term
-
-    def get(self):
-        pass     
