3c8303cc1d7fe6d37eaa2d5b761998f459b3e047
Abhinav Awasthi <abhinavawasthikvgn@gmail.com>
2021-08-10 18:56:28 +0530

    create a layout class for the pages

--- webapp/about.py
+++ webapp/about.py
@@ -9,12 +9,13 @@ class About:
         jp.Div(a = div, text= "About page", 
                 classes = "text-4xl m-2" )
         jp.Div( a = div, text ="""
-        ok 
-        test 
-        sahi 
-        lorem
-        is 
-        not 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum.
         working 
         
         """, classes= "text-lg")
--- webapp/home.py
+++ webapp/home.py
@@ -1,22 +1,29 @@
 import justpy as jp
-
+from webapp.layout import DefaultLayout
 
 class Home:
     path= "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind= True)
-        div = jp.Div(a=wp,  classes = "bg-gray-200 h-screen")
+
+        lay = DefaultLayout(a=wp, view = "hHh lpR fFf")
+        container = jp.QPageContainer(a=lay)
+
+        div = jp.Div(a=container,  classes = "bg-gray-200 h-screen")
         jp.Div(a = div, text= "Home page", 
                 classes = "text-4xl m-2" )
         jp.Div( a = div, text ="""
-        ok 
-        test 
-        sahi 
-        lorem
-        is 
-        not 
-        working 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+        PageMaker including versions of Lorem Ipsum.
         
-        """, classes= "text-lg")
+        """, classes= "text-lg p-2")
         return wp
+
+    
